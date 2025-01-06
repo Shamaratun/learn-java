5193d0404a3fb16f38b1b1b6a73ac1b04e9d784e
Shamaratun <shamaratunmeme@gmail.com>
2025-01-06 07:29:09 +0600

    6.1.25

---
+++ src/InsertionSort.java
@@ -0,0 +1,17 @@
+package twenty4thPractice;
+
+import java.io.OptionalDataException;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] list = {2,5,6,7,8,22,23,45,18,29,32};
+                for(int i = 1; i<list.length; i++){
+            int currentElement = list[i];
+            int k;
+            for(k=i-1;k>=0 && list[k]>currentElement;k--){
+                list[k+1]= list[k];
+
+                    }
+            list[k+1] = currentElement;
+        }
+    }}
